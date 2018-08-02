4ace720fb9f208181f7d5f8ba04701682343b198
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-02 18:19:36 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- CSPP-1 Assignments/p2/bob_counter.py
+++ CSPP-1 Assignments/p2/bob_counter.py
@@ -9,10 +9,10 @@ def main():
     '''This function is used to count number of times bob occured '''
     s_tr = input()
     cou_nt = 0
-    for i in range(0,len(s_tr)-2):
+    for i in range(0, len(s_tr)-2):
         if(s_tr[i] == 'b' and s_tr[i+1] == 'o' and s_tr[i+2] == 'b'):
             cou_nt += 1
     print(cou_nt)
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
