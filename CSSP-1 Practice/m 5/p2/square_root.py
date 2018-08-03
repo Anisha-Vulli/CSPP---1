8c35575b1405e3ec4c065f3cb0a9632434e3e405
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-03 17:56:32 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- CSSP-1 Practice/m 5/p2/square_root.py
+++ CSSP-1 Practice/m 5/p2/square_root.py
@@ -10,13 +10,13 @@ def main():
     s_q = input()
     ep_i = 0.01
     gu_ess = 0.0
-    inc_e = 0.001
+    inc_e = 0.1
     while abs(gu_ess ** 2 - int(s_q)) >= ep_i and gu_ess <= int(s_q):
         gu_ess += inc_e
     if abs(gu_ess ** 2 - int(s_q)) >= ep_i:
         print('Failed on cube root of' + str(s_q))
     else:
-        print(gu_ess, 'is close to the cube root of', str(s_q))
+        print(gu_ess)
 
 if __name__ == "__main__":
     main()
