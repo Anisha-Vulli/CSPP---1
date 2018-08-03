24213afd1d6331fd55637115ed77b6e99f907e8c
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-03 15:09:12 +0530

    " -> 2 of 4 passed. pylint: 10.00/10 "

--- CSSP-1 Practice/m 5/p1/perfect_cube.py
+++ CSSP-1 Practice/m 5/p1/perfect_cube.py
@@ -8,13 +8,13 @@ Date : 03 Aug 2018
 def main():
     ''' This function is used to caluclate cube root through guess and check '''
     #print("Enter an integer")
-    x = input()
+    x_i = input()
     res = 0
-    while res**3 < int(x):
+    while res**3 < int(x_i):
         res = res + 1
-    if res**3 == int(x):
-        print(str(x) + " is a perfect cube")
+    if res**3 == int(x_i):
+        print(str(x_i) + " is a perfect cube")
     else:
-        print(str(res) + "is not a perfect cube")
+        print(str(x_i) + "is not a perfect cube")
 if __name__ == "__main__":
     main()
