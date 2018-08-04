85e5197d913b08521fb6e5b2a84e2b7e931671c8
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-04 11:24:14 +0530

    " -> 4 of 7 passed. pylint: 7.00/10 "

--- CSPP-1 Assignments/m6/p2/special_char.py
+++ CSPP-1 Assignments/m6/p2/special_char.py
@@ -12,7 +12,7 @@ def main():
     Put spaces in place of special characters
     '''
     str_input = input()
-    str_output = " "
+    str_output = ""
     #a = {'!','@','#','$','%','^','&','*'}
     for char in str_input:
         if char == '!' or char == '@' or char == '#' or char == '$' or char == '%' or char == '*':
