4347eddc55fc9ebbfa07976f8b8ff625d07a0540
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-04 11:13:34 +0530

    " -> 7 of 7 passed. pylint: 8.64/10 "

--- CSPP-1 Assignments/m6/p3/digit_product.py
+++ CSPP-1 Assignments/m6/p3/digit_product.py
@@ -1,6 +1,6 @@
 '''
 Given a  number int_input, find the product of all the digits
-example: 
+example:
 	input: 123
 	output: 6
 '''
@@ -9,25 +9,28 @@ def main():
     Finding product of all digits
     '''
     int_input = int(input())
-    count_i = 0
-    n = 0
-    d = 0
+    #count_i = 0
+    n_i = 0
+    d_i = 0
     pro_o = 1
-    if n>0:
-        n = int_input
-        while n>0:
-            d = n % 10
-            n = n //10
-            pro_o = pro_o * d
-        print(pro_o)
-    else:
-        n = -(int_input)
-        while n>0:
-            d = n % 10
-            n = n //10
-            pro_o = pro_o * d
-        print(-(pro_o))
+    if int_input != 0:
+        if  int_input > 0:
+            n_i = int_input
+            while n_i > 0:
+                d_i = n_i % 10
+                n_i = n_i //10
+                pro_o = pro_o * d_i
+            print(pro_o)
+        else:
+            n_i = -(int_input)
+            while n_i > 0:
+               d_i = n_i % 10
+               n_i = n_i //10
+               pro_o = pro_o * d_i
+            print(-(pro_o))
 
+    else:
+        print(0)
 
 if __name__ == "__main__":
     main()
