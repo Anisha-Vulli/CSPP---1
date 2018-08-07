e8814ac0ace9f54ef695dd7986a0271a4efb05b9
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-07 16:43:13 +0530

    poweriter commit

--- CSSP-1 Practice/m8/power using Iteration/power_iter.py
+++ CSSP-1 Practice/m8/power using Iteration/power_iter.py
@@ -1,23 +1,24 @@
 '''
 
-
-Author: Anisha Vulli
-Date: 07 Aug 2018
+Author : Anisha Vulli
+Date : 07 Aug 2018
 
 '''
-
-def iter_power(base, exp):
-    ''' Power of a number caluculation using iteration'''
+def iterPower(base, exp):
+    '''Caluculate power using iteration '''
     ans = 1
-    while exp > 0:
+    while exp>0:
         ans = ans * base
         exp -= 1
     return ans
-def main():
-    ''' Main function used to read input and print output '''
-    base_num = input()
-    exp_num = input()
-    print(iter_power(int(base_num), int(exp_num)))
+    
 
-if __name__ == "__main__":
+
+def main():
+    ''' Main function which is used to take inputs  '''
+    data = input()
+    data = data.split()
+    print(iterPower(float(data[0]),int(data[1]))) 
+
+if __name__== "__main__":
     main()
