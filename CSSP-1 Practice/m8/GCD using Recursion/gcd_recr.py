696f02a85e4ff01a364525f597417070af5187ef
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-07 18:05:32 +0530

    GCD_recur commit

--- CSSP-1 Practice/m8/GCD using Recursion/gcd_recr.py
+++ CSSP-1 Practice/m8/GCD using Recursion/gcd_recr.py
@@ -7,21 +7,21 @@ Date : 07 Aug 2018
 
 
 def gcd_recur(a_in, b_in):
-    temp_var = 0
-    lim = min(a_in,b_in)
+    ''' This function is used to calculate GCD using recursion'''
+    #temp_var = 0
+    lim = min(a_in, b_in)
     if lim == a_in:
-    	temp_var = a_in
-    	a_in = b_in
-    	b_in = temp_var
+        a_in, b_in = b_in, a_in
     if a_in%b_in == 0:
-    	return b_in
+        return b_in
     return gcd_recur(b_in, a_in%b_in)
 
 
 def main():
+    ''' Main function is used to take input and print output '''
     data = input()
     data = data.split()
-    print(gcd_recur(int(data[0]),int(data[1])))
+    print(gcd_recur(int(data[0]), int(data[1])))
 
 if __name__ == "__main__":
     main()
