47293f8365ff5b65e3fb908ee6427803b7cd71d6
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-07 19:10:37 +0530

    " -> 10 of 10 passed. pylint: 7.78/10 "

--- CSSP-1 Practice/m8/p1/Assig_1.py
+++ CSSP-1 Practice/m8/p1/Assig_1.py
@@ -8,8 +8,7 @@ def factorial_fun(n_in):
     ''' Calculating the factorial of the number '''
     if (n_in == 0) or (n_in == 1):
         return 1
-    else:
-        return n_in * factorial_fun(n_in - 1)
+    return n_in * factorial_fun(n_in - 1)
 def main():
     ''' Main function is used to take input and output '''
     num_in = input()
