72e15a408e78ff185fa418c97034bb927a57b2c6
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-07 19:23:54 +0530

    " -> 10 of 10 passed. pylint: 1.11/10 "

---
+++ CSSP-1 Practice/m8/p2/assign_2.py
@@ -0,0 +1,23 @@
+'''
+
+Author : Anisha Vulli
+Date: 07 Aug 2018
+
+'''
+
+def sum_of_digits(n):
+    ''' Function is used to caluculate sum of digits'''
+    if n == 0:
+       return n
+    else:
+       return (n%10 + sum_of_digits(n//10))
+
+
+def main():
+    ''' Main function to take input and print output '''
+    num_in = input()
+    print(sum_of_digits(int(num_in)))  
+
+if __name__== "__main__":
+    main()
+
