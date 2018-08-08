a40535d24ca6fe8533e7c2dbaf097f213113ae68
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-08 22:44:33 +0530

    " -> 0 of 10 passed. pylint: 1.18/10 "

--- CSSP-1 Practice/m7/Functions - Assignment-1/assignment1.py
+++ CSSP-1 Practice/m7/Functions - Assignment-1/assignment1.py
@@ -6,18 +6,17 @@ Date: 08 Aug 2018
 
 '''
 
-def payingDebtOffInAYear(balance, annual_interest_rate, monthly_payment_rate):
-    ''' Function used to calucuate credit card balance ''' 
+def paying_debt_off_in_a_year(balance, annual_interest_rate, monthly_payment_rate):
+    '''function to find the remaining credit card balance'''
     i = 1
-    prev_balance = balance
-    while i <= 12:
+    balance_copy = balance
+    while i < 12:
         monthly_intr_rate = annual_interest_rate / 12.0
-        min_monthly_pay = monthly_payment_rate * prev_balance
-        monthly_unpaid_bal = prev_balance - min_monthly_pay
+        min_monthly_pay = monthly_payment_rate * balance_copy
+        monthly_unpaid_bal = balance_copy - min_monthly_pay
         balance_copy = monthly_unpaid_bal + (monthly_intr_rate * monthly_unpaid_bal)
         i += 1
     return "Remaining balance: " + str(round(balance_copy, 2))
-
 
 def main():
 	''' Main function '''
