7e6435d77627cc76b2462ba4d8d1ddfbe92ef549
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-08 23:19:30 +0530

    fourth_power commit

---
+++ CSSP-1 Practice/m7/Functions - Fourth Power Exercise/fourth_power.py
@@ -0,0 +1,24 @@
+# Exercise: fourth power
+
+# Write a Python function, fourthPower, that takes in one number and returns that value raised to the fourth power.
+
+# You should use the square procedure that you defined in an earlier exercise exercise (you don't need to redefine square in this box;
+
+# This function takes in one number and returns one number.
+
+def square(x_in):
+    return x_in ** 2
+def fourthPower(x_in):
+    return square(x_in)** 2
+   
+def main():
+    data = input()
+    data = float(data)
+    temp = str(data).split('.')
+    if(temp[1] == '0'):
+        print(fourthPower(int(float(str(data)))))
+    else:
+        print(fourthPower(data))
+
+if __name__== "__main__":
+    main()
