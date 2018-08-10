366dd24a5340edc08880bc26b5c2b5d3c469d132
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-10 17:30:08 +0530

    " -> 0 of 6 passed. pylint: 5.79/10 "

--- CSSP-1 Practice/m11/p2/assignment2.py
+++ CSSP-1 Practice/m11/p2/assignment2.py
@@ -1,20 +1,20 @@
 '''
 #Exercise: Assignment-2
-#Implement the updateHand function. Make sure this function has no side effects: i.e., 
-it must not mutate the hand passed in. 
-Before pasting your function definition here, 
+#Implement the updateHand function. Make sure this function has no side effects: i.e.,
+it must not mutate the hand passed in.
+Before pasting your function definition here,
 be sure you've passed the appropriate tests in test_ps4a.py.
 
 Author : Anisha Vulli
 Date : 10 Aug 2018
 
 '''
-def updateHand(hand, word):
+def update_hand(hand, word):
     """
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
     a letter appears in 'word', 'hand' has at least as
-    many of that letter in it. 
+    many of that letter in it.
 
     Updates the hand: uses up the letters in the given word
     and returns the new hand, without those letters in it.
@@ -22,12 +22,13 @@ def updateHand(hand, word):
     Has no side effects: does not modify hand.
 
     word: string
-    hand: dictionary (string -> int)    
+    hand: dictionary (string -> int)
     returns: dictionary (string -> int)
     """
     # TO DO ... <-- Remove this comment when you code this function
 
     #full_letters = "abcdefghijklmnopqrstuvwxyz"
+    ''' Update hand function '''
     given_word = word
     given_dict = hand
     #updated_dict = {}
@@ -36,23 +37,20 @@ def updateHand(hand, word):
     for i in given_word:
         if i in given_dict:
             given_dict[i] -= 1
-        
 
     return given_dict
 
-    
-
 def main():
-	n=input()
-	adict={}
-	for i in range(int(n)):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	data1=input()
-	print(updateHand(adict,data1))
-		
+    ''' Main function '''
+    num_in = input()
+    adict = {}
+    for i in range(int(num_in)):
+        data = input()
+        l = data.split()
+        adict[l[0]] = int(l[1])
+    data1 = input()
+    print(updateHand(adict, data1))
 
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
