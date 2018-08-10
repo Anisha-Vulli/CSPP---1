9e9d115c49aba5a83ec98ffceacb8d865c769c24
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-10 18:59:19 +0530

    " -> 5 of 5 passed. pylint: 8.75/10 "

--- CSSP-1 Practice/m11/p4/assignment4.py
+++ CSSP-1 Practice/m11/p4/assignment4.py
@@ -6,16 +6,15 @@ Date: 10 Aug 2018
 '''
 
 def calculate_hand_len(hand):
-    ''' Hand length function ''' 
-    count = 0
+    ''' Hand length function '''
+    
     hand_list = list(hand.values())
     #print(hand_list)
-    sum =0
+    sum_val = 0
     #print(hand.values())
     for i in hand_list:
-        sum = sum +i
-    
-    return sum
+        sum_val = sum_val +i
+    return sum_val
 
 def main():
     ''' Main function '''
@@ -23,10 +22,10 @@ def main():
     a_dict = {}
     for i in range(int(n_in)):
         data = input()
-        l = data.split()
-        a_dict[l[0]] = int(l[1])
+        l_i = data.split()
+        a_dict[l_i[0]] = int(l_i[1])
     print(calculate_hand_len(a_dict))
-        
+
 
 
 if __name__ == "__main__":
