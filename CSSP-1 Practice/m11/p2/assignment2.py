eb5ef02239176d4e5b7e2741caf3eb6ebf1ec0cd
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-10 17:32:31 +0530

    " -> 6 of 6 passed. pylint: 9.44/10 "

--- CSSP-1 Practice/m11/p2/assignment2.py
+++ CSSP-1 Practice/m11/p2/assignment2.py
@@ -10,21 +10,6 @@ Date : 10 Aug 2018
 
 '''
 def update_hand(hand, word):
-    """
-    Assumes that 'hand' has all the letters in word.
-    In other words, this assumes that however many times
-    a letter appears in 'word', 'hand' has at least as
-    many of that letter in it.
-
-    Updates the hand: uses up the letters in the given word
-    and returns the new hand, without those letters in it.
-
-    Has no side effects: does not modify hand.
-
-    word: string
-    hand: dictionary (string -> int)
-    returns: dictionary (string -> int)
-    """
     ''' Update hand function '''
     given_word = word
     given_dict = hand
@@ -43,8 +28,8 @@ def main():
     adict = {}
     for i in range(int(num_in)):
         data = input()
-        l = data.split()
-        adict[l[0]] = int(l[1])
+        l_i = data.split()
+        adict[l_i[0]] = int(l_i[1])
     data1 = input()
     print(update_hand(adict, data1))
 
