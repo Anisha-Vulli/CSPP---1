8d09a478de1d444f29152c519ea6ab7719abbe8e
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-17 14:58:08 +0530

    " -> 2 of 3 passed. pylint: 9.86/10 "

--- CSSP-1 Practice/m - 16/CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance_cal.py
+++ CSSP-1 Practice/m - 16/CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance_cal.py
@@ -10,6 +10,9 @@ def similarity(dict1, dict2, dict3):
     '''
     #a = len(dict1)
     num_val = 0
+    a_1 = 0
+    b_1 = 0
+    distance = 0
     for i in dict1:
         num_val += dict1[i][0] * dict1[i][1]
     #print(num)
