904a0be4e0a49a59e8ece23d604b9321ab2b9c19
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-17 14:53:49 +0530

    " -> 2 of 3 passed. pylint: 9.85/10 "

--- CSSP-1 Practice/m - 16/CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance.py
+++ CSSP-1 Practice/m - 16/CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance.py
@@ -82,7 +82,7 @@ def freq_count(list_1, list_2):
             common_dict[i] = [freq_count_dict_1[i], freq_count_dict_2[i]]
         else:
             common_dict[i] = [freq_count_dict_1[i], 0]
-  
+
     for p_1 in common_dict:
         if p_1 not in common_dict:
             common_dict[p_1] = [0, freq_count_dict_2[p_1]]
