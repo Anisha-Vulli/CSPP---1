086a6b33fc9c42908e3abb441b6b6bded247b9a9
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-17 16:34:03 +0530

    " -> 1 of 3 passed. pylint: 9.05/10 "

--- CSSP-1 Practice/m - 16/CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance_cal.py
+++ CSSP-1 Practice/m - 16/CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance_cal.py
@@ -22,7 +22,7 @@ def similarity(dict1, dict2, dict3):
     for k in dict3:
         b_1 += dict3[k] ** 2
 
-    distance = (num_val) / math.sqrt(a_1*b_1)
+    distance = (num_val) / math.sqrt(a_1) * math.sqrt(b_1)
     return distance
 
 def load_stopwords(file_name):
