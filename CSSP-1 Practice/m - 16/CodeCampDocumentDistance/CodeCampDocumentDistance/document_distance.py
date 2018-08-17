1258392c1be9b44986c0c6bc819792f4200a4f61
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-17 14:52:18 +0530

    " -> 0 of 3 passed. pylint: 8.48/10 "

--- CSSP-1 Practice/m - 16/CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance.py
+++ CSSP-1 Practice/m - 16/CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance.py
@@ -9,18 +9,18 @@ def similarity(dict1, dict2, dict3):
         Compute the document distance as given in the PDF
     '''
     #a = len(dict1)
-    num = 0
+    num_val = 0
     for i in dict1:
-        num = dict1[i][0] * dict1[i][1]
+        num_val = dict1[i][0] * dict1[i][1]
     #print(num)
     for j in dict2:
-        a = dict2[j] ** 2
+        a_1 = dict2[j] ** 2
     for k in dict3:
-        b = dict3[k] ** 2
+        b_1 = dict3[k] ** 2
 
-    distance = (num) / math.sqrt(a*b)
+    distance = (num) / math.sqrt(a_1*b_1)
     return distance
-    
+
 def load_stopwords(filename):
     '''
         loads stop words from a file and returns a dictionary
@@ -57,15 +57,13 @@ def word_list(input1, input2):
         for j in list_2:
             if i == j:
                 list_2.remove(j)
-    return(freq_count(list_1, list_2))
+    return freq_count(list_1, list_2)
 
 def freq_count(list_1, list_2):
     '''Creating common dictonary '''
     freq_count_dict_1 = {}
     freq_count_dict_2 = {}
     common_dict = {}
-    sum_val = 0
-    sum_val_1 = 0
 
     for k in list_1:
         if k not in freq_count_dict_1:
@@ -78,18 +76,18 @@ def freq_count(list_1, list_2):
             freq_count_dict_2[k] = 1
         else:
             freq_count_dict_2[k] += 1
-    
+
     for i in freq_count_dict_1:
         if i in freq_count_dict_2:
             common_dict[i] = [freq_count_dict_1[i], freq_count_dict_2[i]]
         else:
-            common_dict[i] = [freq_count_dict_1[i],0]
+            common_dict[i] = [freq_count_dict_1[i], 0]
             
     for p in common_dict:
         if p not in common_dict:
-            common_dict[p] = [0,freq_count_dict_2[p]]
+            common_dict[p] = [0, freq_count_dict_2[p]]
 
-    return(common_dict,freq_count_dict_1,freq_count_dict_2)
+    return(common_dict, freq_count_dict_1, freq_count_dict_2)
 
 
 
@@ -99,7 +97,7 @@ def main():
     '''
     input1 = input()
     input2 = input()
-    (common_dict_1,dict_1,dict_2)= word_list(input1, input2)
+    (common_dict_1, dict_1, dict_2)= word_list(input1, input2)
     print(similarity(common_dict_1, dict_1, dict_2))
 
 if __name__ == '__main__':
