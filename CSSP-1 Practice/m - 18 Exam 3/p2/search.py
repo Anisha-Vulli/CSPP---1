b53698072af19bdda471a349c220d80ef620b596
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-18 12:01:32 +0530

    " -> 0 of 1 passed. pylint: 7.08/10 "

--- CSSP-1 Practice/m - 18 Exam 3/p2/search.py
+++ CSSP-1 Practice/m - 18 Exam 3/p2/search.py
@@ -38,12 +38,18 @@ def search(search_index, query):
         make a set of doc_id and return
     '''
     query_sent = []
+    a = set({})
     for i in query:
         list_1 = i.split(" ")
-        print(list_1)
+        #print(list_1)
         query_sent.append(list_1)
 
-    print(query_sent)
+    for i in query_sent:
+        for j in i:
+            if j in search_index:
+                a = set(search_index[j][0])
+    return a
+
 
 def process_queries(search_index, queries):
     '''
@@ -52,7 +58,7 @@ def process_queries(search_index, queries):
         print the results returned by search function
     '''
     queries_sent = queries
-    search(search_index,queries)
+    print(search(search_index,queries))
 
 def main():
     '''
