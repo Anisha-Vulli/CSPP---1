d79e58cfa0142b3004fe844bf10aa593923098dd
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-23 10:57:59 +0530

    " -> 4 of 8 passed. pylint: -4.44/10 "

--- CSSP-1 Practice/m 20/CodeCampMatrixOperations/matrix_operations.py
+++ CSSP-1 Practice/m 20/CodeCampMatrixOperations/matrix_operations.py
@@ -12,7 +12,7 @@ def mult_matrix(m_1, m_2,n):
     	for j in range(0,n):
     		sum_val = 0
     		for k in range(0,n):
-    			sum_val = sum_val + (m_1[i][k] * m_2[j][k])
+    			sum_val = sum_val + (m_1[i][k] * m_2[k][j])
     		in_mat.append(sum_val)
 
     	res_mat.append(in_mat)
