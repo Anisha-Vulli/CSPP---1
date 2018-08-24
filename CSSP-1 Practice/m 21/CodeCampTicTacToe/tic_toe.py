53afd62480ba185efbce5878cbbb975e347b1726
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-24 15:00:37 +0530

    " -> 13 of 14 passed. pylint: 8.61/10 "

--- CSSP-1 Practice/m 21/CodeCampTicTacToe/tic_toe.py
+++ CSSP-1 Practice/m 21/CodeCampTicTacToe/tic_toe.py
@@ -6,20 +6,19 @@ def is_diagonal_forward(board, turn):
     if count_val == 3:
         #print("DF true")
         return True
-    count_val = 0
     return False
 
 def is_diagonal_backward(board, turn):
     count_val = 0
+    in_loop = 2
     for lop_1 in range(3):
-        for in_loop in range(3):
-            if not board[lop_1][in_loop] is turn:
-                count_val += 1
-            in_loop -= 1
-        if count_val == 0:
-            #print("DB true")
-            return True
-        count_val = 0
+        if board[lop_1][in_loop] is turn:
+            count_val += 1
+        in_loop -= 1
+            
+    if count_val == 3:
+        #print("DB true")
+        return True
     return False
 
 def is_horizontal(board, turn):
@@ -90,13 +89,16 @@ def main():
 
     else:
         turn_x = 'x'
-        boolean_x = (is_vertical(board, turn_x) or is_horizontal(board, turn_x) 
-                     or is_diagonal_forward(board, turn_x) 
+        boolean_x = (is_vertical(board, turn_x) or is_horizontal(board, turn_x)
+                     or is_diagonal_forward(board, turn_x)
                      or is_diagonal_backward(board, turn_x))
+
+        #print(boolean_x)
         turn_o = 'o'
-        boolean_o = (is_vertical(board, turn_o) or is_horizontal(board, turn_o) 
+        boolean_o = (is_vertical(board, turn_o) or is_horizontal(board, turn_o)
                      or is_diagonal_forward(board, turn_o) 
                      or is_diagonal_backward(board, turn_o))
+        #print(boolean_o)
 
         if boolean_x and boolean_o and count_val == 0:
             print("invalid game")
