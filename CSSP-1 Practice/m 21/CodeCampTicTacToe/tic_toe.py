1d72b2cff92fa42d94e6bac7592b762d8d927f54
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-24 12:06:15 +0530

    " -> 0 of 14 passed. pylint: 7.17/10 "

--- CSSP-1 Practice/m 21/CodeCampTicTacToe/tic_toe.py
+++ CSSP-1 Practice/m 21/CodeCampTicTacToe/tic_toe.py
@@ -1,23 +1,75 @@
-def print_board(board):
-    board_list = board
-    for i in range(len(board_list)):
-        return join(board_list[i])
+def is_diagonal_forward(board, turn):
+    count_val = 0
+    for lop_1 in range(3):
+        for in_loop in range(3):
+            if not board[lop_1][lop_1] is turn:
+                count_val += 1
+        if count_val == 0:
+            return True
+        count_val = 0
+    return False
 
+def is_diagonal_backward(board, turn):
+    count_val = 0
+    for lop_1 in range(3):
+        for in_loop in range(3):
+            if not board[lop_1][in_loop] is turn:
+                count_val += 1
+            in_loop -= 1
+        if count_val == 0:
+            return True
+        count_val = 0
+    return False
 
+def is_horizontal(board, turn):
+    count_val = 0
+    for lop_1 in range(3):
+        for in_loop in range(3):
+            if not board[lop_1][in_loop] is turn:
+                count_val += 1
+        if count_val == 0:
+            return True
+        count_val = 0
+    return False
 
+def is_vertical(board, turn):
+    count_val = 0
+    for lop_1 in range(3):
+        for in_loop in range(3):
+            if not board[in_loop][lop_1] is turn:
+                count_val += 1
+        if count_val == 0:
+            return True
+        count_val = 0
+    return False
 
 def read_input():
     board = []
     for i in range(0, 3):
-        board.append(input().split())
-
-    print(print_board)
+        board.append(list(map(str, input().split())))
 
 
 def main():
     '''Main function '''
-    
-    read_input()
+    board = read_input()
+    count_val = 0
+
+    turn_x = 'x'
+    boolean_x = (is_vertical(board, turn_x) or is_horizontal(board, turn_x) 
+                 or is_diagonal_forward(board, turn_x) 
+                 or is_diagonal_backward(board, turn_x))
+    turn_o = 'o'
+    boolean_o = (is_vertical(board, turn_o) or is_horizontal(board, turn_o) 
+                 or is_diagonal_forward(board, turn_o) 
+                 or is_diagonal_backward(board, turn_o))
+
+    if boolean_x and count_val == 0:
+        print(turn_x)
+        count_val += 1
+
+    if boolean_o and count_val == 0:
+        print(turn_o)
+        count_val += 1
 
 if __name__ == '__main__':
     main()
---
+++ CSSP-1 Practice/m 21/CodeCampTicTacToe/tictactoe.py
@@ -0,0 +1,96 @@
+'''
+Author : Praveen
+Date : 24-08-2018
+'''
+def is_diagnol_forward(main_list, turn):
+    '''Checks for diagnol forward match'''
+    cou = 0
+    for lo_op in range(3):
+        if not main_list[lo_op][lo_op] is turn:
+            cou += 1
+    if cou == 0:
+        return True
+    return False
+def is_diagnol_backward(main_list, turn):
+    '''Checks for diagnol backward match'''
+    cou = 0
+    in_loop = 2
+    for lo_op in range(3):
+        if not main_list[lo_op][in_loop] is turn:
+            cou += 1
+        in_loop -= 1
+    if cou == 0:
+        return True
+    return False
+def is_horizontal(main_list, turn):
+    '''Checks for horizontal match'''
+    cou = 0
+    for lo_op in range(3):
+        for in_loop in range(3):
+            if not main_list[lo_op][in_loop] is turn:
+                cou += 1
+        if cou == 0:
+            return True
+        cou = 0
+    return False
+def is_vertical(main_list, turn):
+    '''Checks for Vertical match'''
+    cou = 0
+    for lo_op in range(3):
+        for in_loop in range(3):
+            if not main_list[in_loop][lo_op] is turn:
+                cou += 1
+        if cou == 0:
+            return True
+        cou = 0
+    return False
+def main():
+    '''Main Function'''
+    cou = 0
+    x_count = 0
+    o_count = 0
+    char_count = 0
+    other_char = 0
+    row_one = input().split()
+    row_two = input().split()
+    row_three = input().split()
+    main_list = [row_one, row_two, row_three]
+    for lo_op in range(3):
+        for in_loop in range(3):
+            if main_list[lo_op][in_loop] == 'x':
+                x_count += 1
+            elif main_list[lo_op][in_loop] == 'o':
+                o_count += 1
+            elif main_list[lo_op][in_loop] == '.':
+                char_count += 1
+            else:
+                other_char += 1
+    if other_char != 0:
+        print("invalid input")
+        cou += 1
+    elif x_count > o_count + 1 or o_count > x_count + 1:
+        print("invalid game")
+        cou += 1
+    turn_x = 'x'
+    boolean_x = (is_horizontal(main_list, turn_x)
+                 or is_vertical(main_list, turn_x)
+                 or is_diagnol_forward(main_list, turn_x)
+                 or is_diagnol_backward(main_list, turn_x))
+    turn_o = 'o'
+    boolean_o = (is_horizontal(main_list, turn_o)
+                 or is_vertical(main_list, turn_o)
+                 or is_diagnol_forward(main_list, turn_o)
+                 or is_diagnol_backward(main_list, turn_o))
+    if boolean_x and boolean_o and cou == 0:
+        print("invalid game")
+        cou += 1
+    if boolean_x and cou == 0:
+        print(turn_x)
+        cou += 1
+    if boolean_o and cou == 0:
+        print(turn_o)
+        cou += 1
+    if cou == 0:
+        print("draw")
+if __name__ == '__main__':
+    main()
