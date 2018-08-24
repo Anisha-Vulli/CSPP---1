e9a71eb74c228d047c82c6b830a85b22623b3210
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-24 09:50:59 +0530

    " -> 1 of 1 passed. pylint: 8.70/10 "

--- CSSP-1 Practice/m - 18 Exam 3/p1/build_search_index.py
+++ CSSP-1 Practice/m - 18 Exam 3/p1/build_search_index.py
@@ -87,7 +87,7 @@ def build_search_index(docs):
     for doc_id in range(len(doc_list)) :
         for word in doc_list[doc_id]:
             if word in search_index_dict:
-                search_index_dict[word].append(doc_id,doc_list[doc_id][word])
+                search_index_dict[word].append((doc_id,doc_list[doc_id][word]))
             else:
                 search_index_dict[word] = [(doc_id,doc_list[doc_id][word])]
 
