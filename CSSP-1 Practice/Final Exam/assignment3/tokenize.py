0d3d262f9f3c0bcb8bf41924136bb5df4ea7233c
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-25 11:01:02 +0530

    " -> 0 of 4 passed. pylint: 4.38/10 "

--- CSSP-1 Practice/Final Exam/assignment3/tokenize.py
+++ CSSP-1 Practice/Final Exam/assignment3/tokenize.py
@@ -3,13 +3,20 @@ Write a function to tokenize a given string and return a dictionary with the fre
 each word
 '''
 import collections
+def clean_input(string):
+    final_stng = ""
+    final_stng = (''.join(e for e in string if e.isalnum()))
+    return final_stng
+
 def tokenize(string):
     string_obt = string.split()
-    len_list = len(string_obt)
-    print(string_obt)
-    final = []
-    for i in range(len_list):
-        final[i] = collections.Counter(string_obt[i])
+    clean_stng = clean_input(string_obt)
+    print(clean_stng)
+    # len_list = len(string_obt)
+    # print(string_obt)
+    # final = []
+    # for i in range(len_list):
+    #     final[i] = collections.Counter(string_obt[i])
 
     return final
 def main():
