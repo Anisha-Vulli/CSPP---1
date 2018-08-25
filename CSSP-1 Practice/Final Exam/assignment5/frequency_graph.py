b57c99188834c8f1bc8f8aed57393bd28e21638b
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-25 12:01:28 +0530

    " -> 4 of 4 passed. pylint: 9.33/10 "

--- CSSP-1 Practice/Final Exam/assignment5/frequency_graph.py
+++ CSSP-1 Practice/Final Exam/assignment5/frequency_graph.py
@@ -4,13 +4,14 @@ frequency of each word. Display the frequency values using “#” as a text bas
 '''
 
 def frequency_graph(dictionary):
+    '''Generation of frequency dictonary '''
     temp_keys = sorted(dictionary.keys())
     temp_dict = {}
     for key in temp_keys:
-    	if key in temp_dict:
-    		temp_dict[key].append('#' * dictionary[key])
-    	else:
-    		temp_dict[key] = '#' * dictionary[key]
+        if key in temp_dict:
+            temp_dict[key].append('#' * dictionary[key])
+        else:
+            temp_dict[key] = '#' * dictionary[key]
 
 
     #print(temp_dict)
@@ -20,6 +21,7 @@ def frequency_graph(dictionary):
         print(key, "-", temp_dict[key])
 
 def main():
+    '''Main function '''
     dictionary = eval(input())
     frequency_graph(dictionary)
 
