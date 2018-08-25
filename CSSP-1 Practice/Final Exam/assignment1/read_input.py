573ecc17be193c8f00a0d280742acd18393e5669
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-25 10:35:43 +0530

    " -> 2 of 2 passed. pylint: 6.00/10 "

---
+++ CSSP-1 Practice/Final Exam/assignment1/read_input.py
@@ -0,0 +1,17 @@
+'''
+Write a python program to read multiple lines of text input and store the input into a string.
+'''
+
+def main():
+    number_lines = int(input())
+    final_stng = ""
+    temp_stng = ""
+    for i in range(number_lines):
+    	temp_stng = input() + '\n'
+    	final_stng = final_stng + temp_stng
+
+    print(final_stng)
+
+
+if __name__ == '__main__':
+    main()
