2905eea4002320f61fc53a1d34dd465ebc32935d
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-25 10:51:58 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

--- CSSP-1 Practice/Final Exam/assignment3/tokenize.py
+++ CSSP-1 Practice/Final Exam/assignment3/tokenize.py
@@ -2,12 +2,18 @@
 Write a function to tokenize a given string and return a dictionary with the frequency of
 each word
 '''
-
+import collections
 def tokenize(string):
-    pass
-            
+	string_obt = string
+	final = {}
+	for i in string:
+		final = collections.Counter(string)
+
+    return final
 def main():
-    pass
+    range_num = int(input())
+    for _ in range(range_num):
+    	print(tokenize(input()))
 
 if __name__ == '__main__':
     main()
