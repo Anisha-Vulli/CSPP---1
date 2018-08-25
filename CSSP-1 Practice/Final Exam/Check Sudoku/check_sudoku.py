432dcaee88939394f7e25153fcd735b444c1a395
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-25 12:08:07 +0530

    " -> 4 of 26 passed. pylint: 8.00/10 "

--- CSSP-1 Practice/Final Exam/Check Sudoku/check_sudoku.py
+++ CSSP-1 Practice/Final Exam/Check Sudoku/check_sudoku.py
@@ -13,7 +13,13 @@ def check_sudoku(sudoku):
         Your solution goes here. You may add other helper functions as needed.
         The function has to return True for a valid sudoku grid and false otherwise
     '''
-    print(sudoku)
+    list_sudoku = sudoku
+    for i in list_sudoku:
+        for j in i:
+            if j in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
+                return True
+
+    return False
 
 def main():
     '''
