3977e36e61b68f0f77278ce59c165db2cf9aa2b2
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-25 10:36:41 +0530

    " -> 2 of 2 passed. pylint: 10.00/10 "

--- CSSP-1 Practice/Final Exam/assignment1/read_input.py
+++ CSSP-1 Practice/Final Exam/assignment1/read_input.py
@@ -3,12 +3,13 @@ Write a python program to read multiple lines of text input and store the input 
 '''
 
 def main():
+    '''Reading multiple lines and printing them '''
     number_lines = int(input())
     final_stng = ""
     temp_stng = ""
-    for i in range(number_lines):
-    	temp_stng = input() + '\n'
-    	final_stng = final_stng + temp_stng
+    for _ in range(number_lines):
+        temp_stng = input() + '\n'
+        final_stng = final_stng + temp_stng
 
     print(final_stng)
 
