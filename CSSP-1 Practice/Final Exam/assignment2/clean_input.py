f76549f17814be899523261897eb97a470b2b080
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-25 10:47:45 +0530

    " -> 3 of 3 passed. pylint: 8.89/10 "

--- CSSP-1 Practice/Final Exam/assignment2/clean_input.py
+++ CSSP-1 Practice/Final Exam/assignment2/clean_input.py
@@ -2,14 +2,16 @@
 Write a function to clean up a given string by removing the special characters and retain 
 alphabets in both upper and lower case and numbers.
 '''
-import re
+#import re
 def clean_string(string):
+    '''Cleaning a string '''
     final_stng = ""
     #final_stng = re.sub('[^a-zA-z0-9]','',string)
     final_stng = (''.join(e for e in string if e.isalnum()))
     return final_stng
 
 def main():
+    '''Input a string '''
     string = input()
     print(clean_string(string))
 
