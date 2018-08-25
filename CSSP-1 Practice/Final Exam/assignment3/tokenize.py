6c2fcc5b4f69b5ccdf03748e2b7f232fd1b8fbe3
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-25 11:49:19 +0530

    " -> 4 of 4 passed. pylint: 8.85/10 "

--- CSSP-1 Practice/Final Exam/assignment3/tokenize.py
+++ CSSP-1 Practice/Final Exam/assignment3/tokenize.py
@@ -5,28 +5,23 @@ each word
 import collections
 import re
 def clean_input(string):
+    '''Cleaning string '''
     final_stng = ""
-    final_stng = re.sub('[^ a-zA-z0-9]',' ',string)
+    final_stng = re.sub('[^ a-zA-z0-9]',' ', string)
     #final_stng = (''.join(e for e in string if e.isalpha()))
     #print(final_stng)
     return final_stng
 
 def tokenize(string):
+    '''Dictonary of words and their frequencies '''
     clean_stng = clean_input(string)
     string_obt = clean_stng.split()
-    #print(string_obt)
-    # len_list = len(string_obt)
-    # print(string_obt)
     temp_list = []
     final_dict = {}
     for i in string_obt:
         temp_list = collections.Counter(string_obt)
-        #temp_list = Counter(temp_list)
-
-    #print(temp_list)
 
     for doc_id, doc in enumerate(temp_list):
-        #print(doc)
         if doc in final_dict:
             final_dict[doc].append(temp_list[doc])
         else:
@@ -34,6 +29,7 @@ def tokenize(string):
 
     return final_dict
 def main():
+    '''Main function '''
     range_num = int(input())
     string = ""
     for _ in range(range_num):
