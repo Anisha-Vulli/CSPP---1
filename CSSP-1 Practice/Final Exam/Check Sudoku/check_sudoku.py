e4b6780256ea1c3007c5c6e634ed30cc2f859f6a
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-25 13:31:03 +0530

    " -> 22 of 26 passed. pylint: 8.67/10 "

--- CSSP-1 Practice/Final Exam/Check Sudoku/check_sudoku.py
+++ CSSP-1 Practice/Final Exam/Check Sudoku/check_sudoku.py
@@ -23,7 +23,7 @@ def check_inside(list_sudoku):
         if empty_list == ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
             count_hori += 1
 
-    print(count_hori)
+    #print(count_hori)
 
     empty_list = []
     temp_list = []
@@ -43,9 +43,9 @@ def check_inside(list_sudoku):
         if empty_list == ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
             count_vert += 1
 
-    print(count_vert)
+    #print(count_vert)
 
-    if (count_hori == 9 and count_vert == 9):
+    if (count_hori == 9 or count_vert == 9):
         return True
 
 def check_sudoku(sudoku):
