3571b5827b62dd68fabc29e41bf420ede2d4df60
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-25 10:52:16 +0530

    " -> 0 of 4 passed. pylint: 6.92/10 "

--- CSSP-1 Practice/Final Exam/assignment3/tokenize.py
+++ CSSP-1 Practice/Final Exam/assignment3/tokenize.py
@@ -4,16 +4,16 @@ each word
 '''
 import collections
 def tokenize(string):
-	string_obt = string
-	final = {}
-	for i in string:
-		final = collections.Counter(string)
+    string_obt = string
+    final = {}
+    for i in string:
+        final = collections.Counter(string)
 
     return final
 def main():
     range_num = int(input())
     for _ in range(range_num):
-    	print(tokenize(input()))
+        print(tokenize(input()))
 
 if __name__ == '__main__':
     main()
